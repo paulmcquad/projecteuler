f0b5f80dc602fea3ed63829f175c4bc386c33e61
Paul McQuade <paulmcquad@gmail.com>
2014-09-23 15:03:30 +0100

    Cleaners 3

--- prime.py
+++
@@ -1,9 +0,0 @@
-import sys
-import math
-import random
-	 
-def isprime(num):
-    for j in range(2,int(math.sqrt(num)+1)):
-        if (num % j) == 0:
-            return False
-    return True
--- problem48.py
+++
@@ -1,6 +0,0 @@
-#http://blog.dreamshire.com/project-euler-48-solution/
-L = 1000    
-d = 10    # last d digits of series
-s = sum(pow(n, n, 10**d) for n in range(1, L+1))
- 
-print "Project Euler 48 Solution = %010d" % (s % 10**d)
